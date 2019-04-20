8baab247c1da8e08fcf475502ec0aa30e4105a83
arulkumarkandasamy <karul43@yahoo.co.in>
2019-04-20 01:25:22 +0100

    adding key pair

--- variables.tf
+++ variables.tf
@@ -6,5 +6,4 @@ variable "user_data_file_path" {
 variable "id_rsa_pub" {
   description = "The public key for SSH login."
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCkGfGdKLp3qFDS2PyTttmWvoOAekm1P+1KR2GFsAAeTHPbGgXAqqL6SdMpupVJtr9tH1kTJOVU980znDMBfRJYTdz2mKYhkBkZ4ztYCd7na2Js/eP4alQDSiu5cXqle7ogNlMvX3bc4PskAoRdUHTKG4ZRMMUCgbJkJU1F25d+Qqu4M1TZE9XM85FaodmvxxTHePqvUM3gWxgGHmsMsvr9o6uwHWG7a4efcWGbCWhRNkWSQugeI5+5yEGA2yFPB6fDESlwvtF2Q2vio40EzR1KkOsyR4j/FfXhPlnqKz6d58WwWaH8oujKwm/t1W0zr2f1CLc5eBZfSu04jHyGJgF arulkumar@arulkumar-Inspiron-3558"
-"
 }
